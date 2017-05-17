261fdeee1da27adffedb6a6439322d13cd54f99b
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2017-05-17 22:29:45 +0200

    angular start

---
+++ Angular-ppis/NwtWeb/scripts/controllers/teamsCtrl.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var NwtWeb = angular.module('NwtWeb');
+
+    NwtWeb.controller('teamsController', ['$rootScope', '$scope', '$location', 'teamService', function ($rootScope, $scope, $location, teamService) {
+
+        $scope.loadUsers = function () {
+            $scope.waitUsers = true;
+            teamService.list("teams",
+                function(data) {
+                    if (data) {
+                        $scope.teams = data;
+                        $scope.waitUsers = false;
+                    } else {
+                        notificationsConfig.error("Error!");
+                    }
+                });
+        };
+        $scope.loadUsers();
+    }]);
+
+}());
